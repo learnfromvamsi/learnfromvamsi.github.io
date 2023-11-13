8fb0e56d86f6551bd9a6c2242daea5751e160715
vamsi <113770873+learnfromvamsi@users.noreply.github.com>
2023-11-13 15:37:45 +0530

    upd

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
  var a=[
     {
-      exp1:"hi this is question is a question is a question is a question",
-      exp2:"hi this is evidence is an evidence is an evidence is an evidence",
-      exp3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
+      pnt1:"hi this is question is a question is a question is a question",
+      pnt2:"hi this is evidence is an evidence is an evidence is an evidence",
+      pnt3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
     },
     {
-      exp1:"hi this is question is a question is a question is a question",
-      exp2:"hi this is evidence is an evidence is an evidence is an evidence",
-      exp3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
+      pnt1:"hi this is question is a question is a question is a question",
+      pnt2:"hi this is evidence is an evidence is an evidence is an evidence",
+      pnt3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
     },
      
     {
-      exp1:"hi this is question is a question is a question is a question",
-      exp2:"hi this is evidence is an evidence is an evidence is an evidence",
-      exp3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
+      pnt1:"hi this is question is a question is a question is a question",
+      pnt2:"hi this is evidence is an evidence is an evidence is an evidence",
+      pnt3:"hi this is conclusion is a conclusion is a conclusion is a conclusion"
     }
     
     ];
@@ -23,15 +23,18 @@
     var Ev=document.getElementById("exp2");
     var Co=document.getElementById("exp3");
 
-      
+    var mag = document.getElementById("magna");
+    mag.addEventListener("click", display)
     
 
-    function display(element){
-       let  c=element.getAttribute("numm")
+    function display(){
+       let  c=mag.getAttribute("numm")
+        let num=parseInt(c);
     
-        Qu.innerHTML= a[c-1].exp1;
-        Ev.innerHTML= a[c-1].exp2;
-        Co.innerHTML= a[c-1].exp3; 
+        Qu.innerHTML= a[num-1].pnt1;
+        Ev.innerHTML= a[num-1].pnt2;
+        Co.innerHTML= a[num-1].pnt3; 
+        
     }
 
     let date= new Date();
